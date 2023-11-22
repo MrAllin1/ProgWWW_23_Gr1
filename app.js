f80b724a9f4bd1a57950746a90fbd7a155bf299d
MrAllin1 <alidemandi@gmail.com>
2023-11-22 16:26:26 +0100

    added scrolling and etc

--- app.js
+++ app.js
@@ -5,3 +5,39 @@ menu.addEventListener('click',function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+document.querySelectorAll('.navbar_link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            // Calculate the offset of the navbar
+            const navbarHeight = document.querySelector('.navbar').offsetHeight;
+
+            // Calculate the exact scroll position considering the header height
+            const targetOffset = targetElement.getBoundingClientRect().top + window.pageYOffset;
+            const adjustedScroll = targetOffset - navbarHeight;
+
+            window.scrollTo({
+                top: adjustedScroll,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('buyHouseBtn').addEventListener('click', function () {
+        window.location.href = '/';
+    });
+
+    document.getElementById('rentHouseBtn').addEventListener('click', function () {
+        window.location.href = '/';
+    });
+
+    document.getElementById('sellHouseBtn').addEventListener('click', function () {
+        window.location.href = '/';
+    });
+});
